87ce1ae5ba773ad6a3a2d3ba7edaae913a8d43ef
Akash Y <akash.yellgetti@bts.com>
2022-12-04 18:29:54 +0530

    unqiue email and mobile no

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -23,7 +23,7 @@ const UserSchema = new mongoose.Schema(
     lastName: { type: String, required: true },
     dob: { type: Date, required: true },
     gender: { type: String, required: true },
-    mobileNo: { type: Number, required: true },
+    mobileNo: { type: Number, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isActive: { type: Number, default: 1 },
