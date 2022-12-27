4f252214f1b57de53eb80a9760ab6c1d4fb60a39
Akash Y <akash.yellgetti@bts.com>
2022-12-27 23:50:44 +0530

    changes for validation

--- src/schema/auth.schema.ts
+++ src/schema/auth.schema.ts
@@ -3,9 +3,9 @@ import { object, string, ref, date, number } from "yup";
 export const auth: any = {
     generateOTP: object({
         body: object({
-            firstName: string().required("First Name is required"),
-            lastName: string().required("Last Name is required"),
-            dob: date().required("Last Name is required"),
+            firstName: string().matches(/^[A-Za-z ]*$/, "Please enter valid First name").max(40).required("First Name is required");
+            lastName: string().matches(/^[A-Za-z ]*$/, "Please enter valid Last name").max(40).required("Last Name is required");
+            dob: date().required("Date of birth is required"),
             gender: string().required("Gender is required"),
             mobileNo: number().required("Mobile Number is required").min(1000000000).max(9999999999),
             type: string().required(),
@@ -20,37 +20,35 @@ export const auth: any = {
     }),
     register: object({
         body: object({
-            firstName: string().required("First Name is required"),
-            lastName: string().required("Last Name is required"),
-            dob: date().required("Last Name is required"),
+            firstName: string().matches(/^[A-Za-z ]*$/, "Please enter valid First name").max(40).required("First Name is required");
+            lastName: string().matches(/^[A-Za-z ]*$/, "Please enter valid Last name").max(40).required("Last Name is required");
+            dob: date().required("Date of birth is required"),
             gender: string().required("Gender is required"),
             mobileNo: number().required("Mobile Number is required").min(1000000000).max(9999999999),
             email: string().email("Must be a valid email").required("Email is required"),
-            password: string().required("Password is required").min(6, "Password is too short - should be 6 chars minimum.")
-                .matches(/^[a-zA-Z0-9_.-]*$/, "Password can only contain Latin letters."),
+            password: string().required("Password is required").min(6, "Password is too short - should be 6 chars minimum."),
             passwordConfirmation: string().oneOf([ref("password"), null], "Passwords must match")
         })
     }),
     login: object({
         body: object({
             email: string().email("Must be a valid email").required("Email is required"),
-            password: string().required("Password is required").min(6, "Password is too short - should be 6 chars minimum.")
+            password: string().required("Password is required").min(6, "Password is too short - should be 6 chars minimum."),
         })
     }),
     forgotPassword: object({
         body: object({
             email: string().email("Must be a valid email").required("Email is required"),
-            dob: date().required("Last Name is required"),
+            dob: date().required("Date of birth is required"),
             mobileNo: number().required("Mobile Number is required").min(1000000000).max(9999999999),
         })
     }),
     changePassword: object({
         body: object({
             email: string().email("Must be a valid email").required("Email is required"),
-            dob: date().required("Last Name is required"),
+            dob: date().required("Date of birth is required"),
             mobileNo: number().required("Mobile Number is required").min(1000000000).max(9999999999),
-            password: string().required("Password is required").min(6, "Password is too short - should be 6 chars minimum.")
-                .matches(/^[a-zA-Z0-9_.-]*$/, "Password can only contain Latin letters."),
+            password: string().required("Password is required").min(6, "Password is too short - should be 6 chars minimum."),
             passwordConfirmation: string().oneOf([ref("password"), null], "Passwords must match")
         })
     }),
