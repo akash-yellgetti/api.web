d8bdc5be42c21425a22b853efc00dd905981414f
Akash Y <akash.yellgetti@bts.com>
2023-01-04 15:02:02 +0530

    changes for upload profilePicture

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -15,24 +15,40 @@ class User {
     // console.log('inputs', inputs);
     // console.log('files', files);
     try {
-
-      const file = request.files[0];
-      const newFileName = new Date().getTime() + "_" + _.toLower(file.originalname);
-      const filesDir: any = path.join(__dirname, '../public/uploads/'+ user._id +'/profile');
-      // check if directory exists
-      if (!fs.existsSync(filesDir)) {
-        // if not create directory
-        fs.mkdirSync(filesDir, { recursive: true });
+      const updateData: any = {};
+      let profilePicture: any = _.find(files, { fieldname: 'profilePicture' });
+      if(profilePicture) {
+        profilePicture = this.saveImage(profilePicture, user);
+        updateData['profilePicture'] = profilePicture;
       }
-      const targetPath = 'uploads/'+ user._id +'/profile/'+newFileName;
-      const newFile = fs.createWriteStream(path.join(__dirname, '../public/'+ targetPath)).write(file.buffer);
-      await userService.updateOne({ _id: user._id }, { profilePicture: targetPath });
-      const payload = { code: 200,  data:  { targetPath }, message: 'User Profile Picture Uploaded' };
+      let profilePictureBackground: any = _.find(files, { fieldname: 'profilePictureBackground' });
+      if(profilePictureBackground) {
+        profilePictureBackground = this.saveImage(profilePictureBackground, user);
+        updateData['profilePictureBackground'] = profilePictureBackground;
+      }
+      if(Object.keys(updateData).length > 0) {
+        await userService.updateOne({ _id: user._id }, updateData);
+      }
+      
+      const payload =  { code: 200,  data:  updateData, message: 'User Profile Picture Uploaded' };
       return new Api(response).success().code(200).send(payload);
     } catch (e) {
       log.error( e);
       return new Api(response).error().code(200).send(e);
     }
+  }
+
+  saveImage = (file: any, user: any) => {
+    const newFileName = new Date().getTime() + "_" + _.toLower(file.originalname);
+    const filesDir: any = path.join(__dirname, '../public/uploads/'+ user._id +'/profile');
+    // check if directory exists
+    if (!fs.existsSync(filesDir)) {
+      // if not create directory
+      fs.mkdirSync(filesDir, { recursive: true });
+    }
+    const targetPath = 'uploads/'+ user._id +'/profile/'+newFileName;
+    const newFile = fs.createWriteStream(path.join(__dirname, '../public/'+ targetPath)).write(file.buffer);
+    return targetPath;
   }
 
   detail = async (request: any, response: express.Response) => {
