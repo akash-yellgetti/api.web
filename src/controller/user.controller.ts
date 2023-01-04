7e1e16e7ea108998909656d616f9f2b0ca22834c
Akash Y <akash.yellgetti@bts.com>
2023-01-04 14:40:09 +0530

    Profile Image upload

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import fs from 'fs';
 import { userService } from "../service";
 import { Api, api, log } from '../utils';
+import _ from 'lodash';
 
 class User {
 
@@ -11,14 +12,21 @@ class User {
     const inputs = { ...request.body, ...request.params};
     const files = request.files;
     log.info('controller.User.profilePicture');
-    console.log('inputs', inputs);
-    console.log('files', files);
+    // console.log('inputs', inputs);
+    // console.log('files', files);
     try {
 
       const file = request.files[0];
-      const newFileName = new Date().getTime() + "_" + file.originalname;
-      const targetPath = path.join(__dirname, '../public/uploads/'+ user._id +'/profile/'+newFileName)
-      const newFile = fs.createWriteStream(targetPath).write(file.buffer);
+      const newFileName = new Date().getTime() + "_" + _.toLower(file.originalname);
+      const filesDir: any = path.join(__dirname, '../public/uploads/'+ user._id +'/profile');
+      // check if directory exists
+      if (!fs.existsSync(filesDir)) {
+        // if not create directory
+        fs.mkdirSync(filesDir, { recursive: true });
+      }
+      const targetPath = 'uploads/'+ user._id +'/profile/'+newFileName;
+      const newFile = fs.createWriteStream(path.join(__dirname, '../public/'+ targetPath)).write(file.buffer);
+      await userService.updateOne({ _id: user._id }, { profilePicture: targetPath });
       const payload = { code: 200,  data:  { targetPath }, message: 'User Profile Picture Uploaded' };
       return new Api(response).success().code(200).send(payload);
     } catch (e) {
