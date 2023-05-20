4e1d4c83a0cad741889c0dcde2283919d470bded
Akash Yellgetti <akash.yellgetti@bts.com>
2023-05-20 19:04:44 +0530

    New Build API

--- src/config/setting.ts
+++ src/config/setting.ts
@@ -1,17 +1,18 @@
 export const setting = {
     program: "api",
     mode: "dev",
-    port: process.env.NODE_PORT || 3001,
+    port: 5001,
     version: "1.0.0",
-    host: process.env.NODE_HOST || "localhost",
+    host: "localhost",
     socket: true,
     db: {
-      host: process.env.NODE_DB_HOST || "localhost",
-      user: process.env.NODE_DB_USER || "",
-      password: process.env.NODE_DB_PASSWORD || "",
-      db: process.env.NODE_DB_NAME || "app"
+      host: "194.195.116.219",
+      user: "admin",
+      password: "passw0rd1",
+      db: "app"
     },
     saltWorkFactor: 10,
     accessTokenTtl: "180m",
-    refreshTokenTtl: "1y"
-};
+    refreshTokenTtl: "1y",
+    privateKey: `samplePrivateKey`,
+  };
