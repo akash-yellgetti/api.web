0ac45d3484ed33a5a6bfba8f063b57a6a26b0a67
Akash Yellgetti <akash.yellgetti@bts.com>
2024-01-26 14:18:59 +0530

    change xlsx library

---
+++ src/utils/excel.util.ts
@@ -0,0 +1,20 @@
+import * as XLSX from 'xlsx';
+import * as _ from 'lodash';
+export class Excel {
+  public static CONVERT_TO_JSON(excelBuffer: any): any {
+    const workbook = XLSX.read(excelBuffer);
+    const sheetNameList = workbook.SheetNames;
+    const json = XLSX.utils.sheet_to_json(workbook.Sheets[sheetNameList[0]], { defval: null });
+    return json;
+  }
+
+  public static MULTIPLE_SHEET_CONVERT_TO_JSON(excelBuffer: any): any {
+    const workbook: any = XLSX.read(excelBuffer);
+    const sheetNameList: any = workbook.SheetNames;
+    const json: any = {};
+    _.each(sheetNameList, (sheetName, key) => {
+      json[sheetName] = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], { defval: null });
+    });
+    return json;
+  }
+}
