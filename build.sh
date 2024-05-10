d6e74755864a64212771f05ed373b66590bcb9b4
Akash Yellgetti <akash.yellgetti@bts.com>
2024-05-10 16:01:50 +0530

    New Build API datetime:- datetime:- 20240510 16:01:50

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
+# Define a timestamp function
+timestamp() {
+  date +"datetime:- %Y%m%d %H:%M:%S" # current time
+}
+
 rm -rf dist
 npm run build
 git add -A
-git commit -m "New Build API"
+git commit -m "New Build API datetime:- $(timestamp)"
